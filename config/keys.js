2735bb07eee51251392b0cd06851d57875b86526
Mariodev27 <mariosantisteban23@gmail.com>
2023-11-02 02:48:56 -0500

    proyecto

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    googleClientID: '1093283678474-cuq2tp7olvs76p2oge22hqt9tk4fu24h.apps.googleusercontent.com',
+    googleClientSecret: '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'
+};
